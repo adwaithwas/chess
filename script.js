d78685a76930dcfaefa9a2f06ff425e8e9aa5f9f
adwaithwas <betteradwaith@gmail.com>
2026-03-28 22:48:42 +0530

    added board state and renderBoard function

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
         const square = document.createElement("div");
 
-        if ((i + j) % 2 === 0) square.classList.add("white");
-        else square.classList.add("black");
+        if ((i + j) % 2 === 0) square.classList.add("black");
+        else square.classList.add("white");
 
         square.classList.add("square");
         board.appendChild(square);
@@ -13,14 +13,84 @@ for (let i = 0; i < 8; i++) {
         square.dataset.row = i;
         square.dataset.col = j;
 
+        const circle = document.createElement("div");
+
         // square.addEventListener("click", (e) => {
         //     let row = Number(e.target.dataset.row);
         //     let col = Number(e.target.dataset.col);
 
-        //     console.log([row, col]);
+        // console.log([i,j]);
+        // console.log(boardState[i,j]);
         // });
     }
 }
+
+
+const boardState = Array(8).fill(null).map(() => Array(8).fill(null));
+
+// #_______________________________________________________________#
+// #_______________________________________________________________#
+
+// filling black side pieces
+boardState[0] = [
+    {type: "rook", color: "black"},
+    {type: "knight", color: "black"},
+    {type: "bishop", color: "black"},
+    {type: "queen", color: "black"},
+    {type: "king", color: "black"},
+    {type: "bishop", color: "black"},
+    {type: "knight", color: "black"},
+    {type: "rook", color: "black"}
+]
+
+boardState[1] = Array(8).fill(null).map(() => ({type: "pawn", color: "black"}));
+
+// filling white side pieces
+boardState[6] = Array(8).fill(null).map(() => ({type: "pawn", color: "white"}));
+
+boardState[7] = [
+    {type: "rook", color: "white"},
+    {type: "knight", color: "white"},
+    {type: "bishop", color: "white"},
+    {type: "queen", color: "white"},
+    {type: "king", color: "white"},
+    {type: "bishop", color: "white"},
+    {type: "knight", color: "white"},
+    {type: "rook", color: "white"}
+]
+
+// #_______________________________________________________________#
+// #_______________________________________________________________#
+
+function getSymbol(piece){
+    const symbols = {
+        pawn: "P",
+        rook: "R",
+        knight: "KN",
+        bishop: "B",
+        queen: "Q",
+        king: "K"
+    };
+
+    return piece.color === "white" ? symbols[piece.type] : symbols[piece.type].toLowerCase();
+}
+
+function renderBoard(){
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        let row = Number(square.dataset.row);
+        let col = Number(square.dataset.col);
+
+        let piece = boardState[row][col];
+        square.textContent = "";
+
+        if(piece){
+            square.textContent = getSymbol(piece);
+        }
+    })
+}
+
+renderBoard();
 
 board.addEventListener("click", (e) => {
     if (!e.target.classList.contains("square")) return;
@@ -28,5 +98,6 @@ board.addEventListener("click", (e) => {
     let row = Number(e.target.dataset.row);
     let col = Number(e.target.dataset.col);
 
-    // console.log([row, col]);
+    console.log([row, col]);
+    console.log(boardState[row][col]);
 });
